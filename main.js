43788c3b9cd51286a4bb9ec3c256e150a02247cc
Aaron Lain <aaron.j.lain@gmail.com>
2020-01-27 21:26:27 -0600

    organization

--- main.js
+++ main.js
@@ -139,10 +139,20 @@ const findMyPies = (e) => {
     }
 }
 
-pieBuilder(pies);
+const events = () => {
+    document.getElementById('Zoe').addEventListener('click', findMyPies);
+    document.getElementById('Luke').addEventListener('click', findMyPies);
+    document.getElementById('Mary').addEventListener('click', findMyPies);
+    document.getElementById('Aaron').addEventListener('click', findMyPies);
+    document.getElementById('All').addEventListener('click', findMyPies);
+}
 
-document.getElementById('Zoe').addEventListener('click', findMyPies);
-document.getElementById('Luke').addEventListener('click', findMyPies);
-document.getElementById('Mary').addEventListener('click', findMyPies);
-document.getElementById('Aaron').addEventListener('click', findMyPies);
-document.getElementById('All').addEventListener('click', findMyPies);
+const init = () => {
+    pieBuilder(pies);
+    events();
+}
+
+init();
+
+
+
